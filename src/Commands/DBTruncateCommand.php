7b41fcfb221b99032c1251bfc31ee81014354314
Sairahcaz <zacharias.creutznacher@gmail.com>
2023-03-12 21:47:32 +0100

    db:truncate with tables argument and foreigen key checks option

--- src/Commands/DBTruncateCommand.php
+++ src/Commands/DBTruncateCommand.php
@@ -11,9 +11,11 @@ class DBTruncateCommand extends Command
 {
     use ConfirmableTrait;
 
-    public $signature = 'db:truncate';
+    protected $signature = 'db:truncate {tables?* : Optionally specify only specific tables to truncate}
+               {--force : Force the operation to run when in production}
+               {--checks=true : Enable or disable foreign key checks during truncation}';
 
-    public $description = 'Truncate all tables';
+    protected $description = 'Truncate all tables';
 
     public function handle(): int
     {
@@ -21,16 +23,33 @@ class DBTruncateCommand extends Command
             return 1;
         }
 
-        $this->components->info('Preparing database.');
+        $foreignKeyChecks = $this->hasOption('checks') && filter_var($this->option('checks'), FILTER_VALIDATE_BOOLEAN);
 
-        collect(Schema::getAllTables())->each(function ($tableDefinition) {
-            $table = current($tableDefinition);
+        if (!$foreignKeyChecks) {
+            $this->components->warn('Disabling foreign key checks!');
+            Schema::disableForeignKeyConstraints();
+        }
+
+        $this->components->info('Start truncating tables.');
+
+        $tables = $this->argument('tables') ?: collect(Schema::getAllTables())->map(function ($tableDefinition) {
+            return current($tableDefinition);
+        });
+
+        collect($tables)->each(function ($table) {
             $this->components->task("Truncating table: $table", function () use ($table) {
                 DB::table($table)->truncate();
             });
         });
 
         $this->newLine();
+
+        $this->components->info('Finished truncating tables.');
+
+        if (!$foreignKeyChecks) {
+            $this->components->warn('Re-enabling foreign key checks!');
+            Schema::enableForeignKeyConstraints();
+        }
 
         return self::SUCCESS;
     }
